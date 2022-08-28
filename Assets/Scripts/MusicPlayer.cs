a7519ba8f5ac18e2fcc8ac5ae7ecf03b9f272373
Vlad Rakhmanin <91677083+toadkarter@users.noreply.github.com>
2022-08-28 16:10:48 +0100

    added bgm

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    
+    
+    private static MusicPlayer _instance;
+
+    public static MusicPlayer instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<MusicPlayer>();
+                DontDestroyOnLoad(_instance.gameObject);
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            if (this != _instance)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
